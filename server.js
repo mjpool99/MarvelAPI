4743bf5664de72328d3c9c7960ea5efbe00f3e0b
Mason <mjpool99@gmail.com>
2021-10-05 19:40:10 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const axios = require('axios').default;
+const crypto = require('crypto');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('static'));
+
+let ts = new Date().getTime();
+const publicKey = process.env.PUBLIC_ACCESS_KEY;
+const privateKey = process.env.PRIVATE_ACCESS_KEY;
+
+const hash = crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
+app.post('/marvelData', (req, res) => {
+    let name = req.body.name;
+    const url = "https://gateway.marvel.com:443/v1/public/characters?ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash + "&name=" + name
+    axios({
+        url: url,
+        responseType: 'json',
+    }
+    ).then(data => res.json(data.data)).catch(error => {
+            console.log(error)
+        })
+})
+
+app.post('/marvelComics', (req, res) => {
+    const comicUrl = "https://gateway.marvel.com:443/v1/public/comics?characters=" + req.body.id + "&ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash   
+     axios({
+        url: comicUrl,
+        responseType: 'json',
+    }).then(data => res.json(data.data)).catch(error => {
+            console.log(error)
+        })
+    })
+
+
+    app.listen(3000, () => {
+        console.log("Server is up and running")
+    })
---
+++ static/app.js
@@ -0,0 +1,58 @@
+let heroSearch = document.getElementById("heroSearch");
+let heroName = document.querySelector('[data-hero-name]');
+let heroBio = document.querySelector('[data-hero-bio]');
+let heroImage = document.querySelector('#heroImage');
+
+heroSearch.addEventListener('keypress', (e) => {
+
+    if (e.key === "Enter") {
+        fetch("/marvelData", {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: e.target.value
+            })
+        }).then(res => res.json()).then(data => {
+            let marvelData = data.data.results[0];
+            heroName.textContent = marvelData.name
+            heroImage.src = marvelData.thumbnail.path + "/portrait_fantastic.jpg"
+            heroBio.textContent = marvelData.description
+            if (data.data.results[0].description === "") {
+                heroBio.textContent = "Description of this character is not available."
+            }
+            console.log(data)
+
+            e.target.value = ""
+
+          fetch("/marvelComics", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify({
+                    id: marvelData.id
+                })
+            }).then(res => res.json())
+            .then(data => {
+                for(x=1 ; x<4 ; x+=1){
+                let comicImage = document.querySelector('.comicImg' + x);
+                let comicTitle = document.querySelector('.comicTitle' + x);
+                let link = document.querySelector('.link' + x);
+                let i = Math.floor(Math.random()*(20 + 1))
+                let marvelComics = data.data.results[i];
+                comicTitle.textContent = marvelComics.title
+                link.href = marvelComics.urls[0].url
+                comicImage.src = marvelComics.thumbnail.path + "/portrait_fantastic.jpg"
+                }
+                console.log(data)
+            })
+        }
+).catch ((error) => {
+        console.error();
+})}
+})
+
